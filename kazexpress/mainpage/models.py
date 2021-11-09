de6ff41d367a464ca7f410074e983cf5ef6bf083
Alashcoder <27535@iitu.edu.kz>
2021-11-10 02:02:01 +0600

    database1

--- kazexpress/mainpage/models.py
+++ kazexpress/mainpage/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 class product (models.Model):
 
-    title=models.CharField(_(""), max_length=50)
+    title=models.CharField(max_length=50)
     description=models.TextField(blank=true)
     photo_prod=models.ImageField(_, upload_to=None, height_field=None, width_field=None, max_length=None)
+    price=models.IntegerField()
+
 
 
 
@@ -17,5 +19,25 @@ class product (models.Model):
     def get_absolute_url(self):
         return reverse("_detail", kwargs={"pk": self.pk})
 )
+class User(models.Model):
+   name=CITextField()
+   email=models.EmailField( max_length=254)
+   phone=models.PhoneNumberField()
+   admin='AD'
+   client='CL'
+   
+
+    
+
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
 
 # Create your models here.
